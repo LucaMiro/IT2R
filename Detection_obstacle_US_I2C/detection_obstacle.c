0b1deaa07dc6cd35488a29bd2709e9aceef09deb
Anatole FOUCHER <159080868+AnatoleFoucher@users.noreply.github.com>
2024-03-14 18:33:13 +0100

    Modification bibliothèque I2C Ultrason
    Ajout de commentaires, modification de fonctions
    Ajout d'une fonction d'adressage I2C pour le SRF10 (ne pas toucher)

--- Detection_obstacle_US_I2C/detection_obstacle.c
+++ Detection_obstacle_US_I2C/detection_obstacle.c
@@ -1,7 +1,6 @@
 #include "stm32f4xx_hal.h"
 #include "Driver_I2C.h"
 
-//utilisation du pilote CMSIS d'ARM
 extern ARM_DRIVER_I2C Driver_I2C1;
 
 //fonction configuration liaison I2C
@@ -15,19 +14,49 @@ int configuration_liaisonI2C(void){
 	Driver_I2C1.Control(ARM_I2C_BUS_SPEED, ARM_I2C_BUS_SPEED_STANDARD);
 	//boucle d'attente qui remplace le BUS CLEAR pour l'instant
 	for(i=0;i<100000;i++);
-	return 0;
+	//return 0;
 }
 
-//fonction test fonctionnement liaison capteur
-//elle nous a permis dans une boucle de connaître les adresses des composants esclaves
-void test(uint8_t adresse){
+void adressage(uint8_t adresseCible, uint8_t adresseComposant){
+	uint8_t tableau_commande[2];
+	uint8_t adresseEsclave = 0x70;
+	if(adresseComposant != 0x00){
+		uint8_t adresseEsclave = adresseComposant;
+	}
+	
+	tableau_commande[0] = 0x00;
+	tableau_commande[1] = 0xA0;
+	
+	Driver_I2C1.MasterTransmit(adresseEsclave,tableau_commande,2,false);
+	while(Driver_I2C1.GetStatus().busy == 1);
+	
+	tableau_commande[1] = 0xAA;
+	Driver_I2C1.MasterTransmit(adresseEsclave,tableau_commande,2,false);
+	while(Driver_I2C1.GetStatus().busy == 1);
+	
+	tableau_commande[1] = 0xA5;
+	Driver_I2C1.MasterTransmit(adresseEsclave,tableau_commande,2,false);
+	while(Driver_I2C1.GetStatus().busy == 1);
+	
+	tableau_commande[1] = adresseCible;
+	Driver_I2C1.MasterTransmit(adresseEsclave,tableau_commande,2,false);
+	while(Driver_I2C1.GetStatus().busy == 1);
+}
+
+//fonction test fonctionnement des capteurs avec leur adresse
+//à mettre dans une boucle infinie c'est mieux
+void test_adresse(uint8_t adresse){
+	static int i=0;
 	uint8_t tableau_commande[2];
 	tableau_commande[0] = 0x00;
 	tableau_commande[1] = 0x51;
-	
-	Driver_I2C1.MasterTransmit(adresse,tableau_commande,2,false);
-	while(Driver_I2C1.GetStatus().busy == 1);
+	for(adresse = 0x70; adresse < 0x77; adresse += 0x01){
+		Driver_I2C1.MasterTransmit(adresse,tableau_commande,2,false);
+		while(Driver_I2C1.GetStatus().busy == 1);
+	}
+	for(i=0;i<1000000;i++);
 }
+
 //fonction de configuration de la portée
 void config_portee(uint8_t portee, uint8_t adresse){
 	uint8_t tableau_commande[2];
@@ -36,13 +65,12 @@ void config_portee(uint8_t portee, uint8_t adresse){
 	
 	Driver_I2C1.MasterTransmit(adresse,tableau_commande,2,false);
 }
+
 //fonction mesure de la distance en cm
-uint8_t mesure_cm(uint8_t adresse){
-	int i=0;
-	//définition de l'adresse du capteur lu
-	//registres comportant la valeur de la mesure du SRF10
-	uint8_t registreLow = 0x02;
-	uint8_t registreHigh = 0x03; 
+uint16_t mesure_cm(uint8_t adresse){
+	//registre comportant la valeur "utile" de la mesure du SRF10
+	uint8_t registreLow = 0x03;
+	
 	//initialisation du tableau d'envoi de commande
 	uint8_t tableau_commande[2];
 	uint8_t lecture = 0, lecture2 = 0;
@@ -59,13 +87,13 @@ uint8_t mesure_cm(uint8_t adresse){
 	lecture = 0xFF;
 	//boucle d'attente de la fin de la mesure, lecture continue
 	do{
-		Driver_I2C1.MasterTransmit(adresse,&registreHigh,1,true);
+		Driver_I2C1.MasterTransmit(adresse,&registreLow,1,true);
 		while(Driver_I2C1.GetStatus().busy == 1);
 		Driver_I2C1.MasterReceive(adresse,&lecture,1,false);
 		while(Driver_I2C1.GetStatus().busy == 1);
 	}while(lecture == 0xFF);
 	//rangement de la mesure dans la variable lecture2
-	Driver_I2C1.MasterTransmit(adresse,&registreLow,1,true);
+  Driver_I2C1.MasterTransmit(adresse,&registreLow,1,true);
 	while(Driver_I2C1.GetStatus().busy == 1);
 	Driver_I2C1.MasterReceive(adresse,&lecture2,1,false);
 	while(Driver_I2C1.GetStatus().busy == 1);
--- Detection_obstacle_US_I2C/detection_obstacle.h
+++ Detection_obstacle_US_I2C/detection_obstacle.h
@@ -2,11 +2,10 @@
 
 //définition adresse/numéro capteurs
 #define CAPTEUR_E0 0x70 //emplacement :
+#define CAPTEUR_E2 0x71 //emplacement :
 #define CAPTEUR_E4 0x72 //emplacement :
 #define CAPTEUR_E6 0x73 //emplacement :
 #define CAPTEUR_E8 0x74 //emplacement :
-#define CAPTEUR_YY 0xYY //emplacement :
-#define CAPTEUR_ZZ 0xZZ //emplacement :
 
 //définition de la portée
 #define PORTEE_MAX 0xFF
@@ -14,11 +13,14 @@
 #define PORTEE_4M 0x5D
 #define PORTEE_2M 0x2F
 #define PORTEE_1M 0x18
+
+//fonctions I2C / Ultrason
 int configuration_liaisonI2C(void);
 
+//NE PAS MANIPULER CETTE FONCTION SVP
+void adressage(uint8_t adresseCible, uint8_t adresseComposant);
+
 void config_portee(uint8_t portee, uint8_t adresse);
-
 void test(uint8_t adresse);
-
-uint8_t mesure_cm(uint8_t adresse);
+uint8_t mesure_cm(uint8_t adresse, uint8_t adresseComposant);
 
