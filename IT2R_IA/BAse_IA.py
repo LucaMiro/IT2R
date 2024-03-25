13d95f0d5920fb83adbca3ba3699709304003b62
Badrr12 <144567871+Badrr12@users.noreply.github.com>
2024-03-25 09:31:35 +0100

    détection + performante et envoi de bit d'arret

--- IT2R_IA/BAse_IA.py
+++ IT2R_IA/BAse_IA.py
@@ -1,5 +1,10 @@
 #========================== Programme Base TP2 ==== HD 2024 ======================================
 
+
+from fpioa_manager import fm
+from Maix import GPIO
+
+import time
 import sensor, image, lcd
 import KPU as kpu
 
@@ -17,9 +22,12 @@ sensor.set_windowing((224,224))
 
 #---------- Initialisation KPU avec le modèle IA entrainé au préalable -------------------------
 
+fm.register(15, fm.fpioa.GPIO0) # Attribue la pin IO15 à la fonction logicielle GPIO0
+PIN_STOP = GPIO(GPIO.GPIO0, GPIO.OUT)
+
 modele = kpu.load(0x300000)   # Chargement des coefficients du modèle depuis l'adresse 0x300000
 
-anchors = ( 0.75,0.72,3.89,3.88,2.95,2.81,1.97,2.03,1.06,1.06)
+anchors = (3.19,3.12,2.19,2.19,0.83,0.88,4.28,4.06,1.53,1.61)
 
 kpu.init_yolo2(modele, 0.5, 0.3, 5, anchors) # Initialisation du calculateur IA
 
@@ -28,12 +36,19 @@ kpu.init_yolo2(modele, 0.5, 0.3, 5, anchors) # Initialisation du calculateur IA
 #-------- Boucle principale : Capture image -> Calcul IA -> Affichage résultats ---------------
 
 while True:
+
         img = sensor.snapshot()  # Capture d'une image
         Resultats = kpu.run_yolo2(modele, img)  # Récupération du résultat du calculateur IA
-        if Resultats :
 
+        if Resultats :
+            PIN_STOP.value(1)
             for i in Resultats :  # il peut y avoir plusieurs identifications
                 img.draw_rectangle(i.rect(),color=(0,255,0)) # Tracé de la boite englobante
                 print(i)    # Sur terminal série, affichage du contenu de chaque résultat
+
         lcd.display(img)
+
+        PIN_STOP.value(0)
+
+
 
