d1adddf1973e69024810196682189ce6d289960c
LucaMiro <158859091+LucaMiro@users.noreply.github.com>
2024-04-04 18:12:32 +0200

    DFPlayer+CAN fonctionel , rec

--- IT2R-DFPlayer_Mini/Thread.c
+++ IT2R-DFPlayer_Mini/Thread.c
@@ -39,24 +39,6 @@ void myCAN1_callback(uint32_t obj_idx, uint32_t event)
     }
 }
 
-// CAN2 utilisé pour émission
-void myCAN2_callback(uint32_t obj_idx, uint32_t event)
-{
-    switch (event)
-    {
-    case ARM_CAN_EVENT_SEND_COMPLETE:
-        /* 	Message was sent successfully by the obj_idx object.  */
-        osSignalSet(id_CANthreadT, 0x01);
-        break;
-    }
-}
-
-
-
-
-
-
-
 // CAN1 utilisé pour réception
 void InitCan1 (void) {
 	
@@ -73,7 +55,7 @@ void InitCan1 (void) {
                           
 	// Mettre ici les filtres ID de réception sur objet 0
 	//....................................................
-	Driver_CAN1.ObjectSetFilter(0,ARM_CAN_FILTER_ID_MASKABLE_ADD, ARM_CAN_STANDARD_ID(0xFFF),0x000);
+	Driver_CAN1.ObjectSetFilter(0,ARM_CAN_FILTER_ID_EXACT_ADD, ARM_CAN_STANDARD_ID(0x01),0);
 		
 	Driver_CAN1.ObjectConfigure(0,ARM_CAN_OBJ_RX);				// Objet 0 du CAN1 pour réception
 	
@@ -84,25 +66,6 @@ void InitCan1 (void) {
 
 
 
-
-// CAN2 utilisé pour émission
-void InitCan2 (void) {
-	Driver_CAN2.Initialize(NULL,myCAN2_callback);
-	Driver_CAN2.PowerControl(ARM_POWER_FULL);
-	
-	Driver_CAN2.SetMode(ARM_CAN_MODE_INITIALIZATION);
-	Driver_CAN2.SetBitrate( ARM_CAN_BITRATE_NOMINAL,
-													125000,
-													ARM_CAN_BIT_PROP_SEG(5U)   |         // Set propagation segment to 5 time quanta
-                          ARM_CAN_BIT_PHASE_SEG1(1U) |         // Set phase segment 1 to 1 time quantum (sample point at 87.5% of bit time)
-                          ARM_CAN_BIT_PHASE_SEG2(1U) |         // Set phase segment 2 to 1 time quantum (total bit is 8 time quanta long)
-                          ARM_CAN_BIT_SJW(1U));                // Resynchronization jump width is same as phase segment 2
-	
-	Driver_CAN2.ObjectConfigure(1,ARM_CAN_OBJ_TX);				// Objet 1 du CAN2 pour emission
-	
-	Driver_CAN2.SetMode(ARM_CAN_MODE_NORMAL);					// fin init
-	
-}
 
 
 
@@ -126,7 +89,7 @@ int main (void) {
   // example: tid_name = osThreadCreate (osThread(name), NULL);
 	id_CANthreadR = osThreadCreate (osThread(CANthreadR), NULL);
 	id_CANthreadT = osThreadCreate (osThread(CANthreadT), NULL);
-  tid_Thread1 = osThreadCreate (osThread(Thread1), NULL);
+  //tid_Thread1 = osThreadCreate (osThread(Thread1), NULL);
   if (!tid_Thread1) return(-1);
 	
   osKernelStart ();                         // start thread execution 
@@ -167,18 +130,15 @@ void CANthreadR(void const *argument)
 	char texte[2];
 	init_UART2();
 	
-		osDelay(100);
+	osDelay(100);
 	while(1)
 	{		
 		
 		osSignalWait(0x01, osWaitForever);		// sommeil en attente réception
-		Driver_CAN1.MessageRead(0,&rx_msg_info,data_buf,8);
+		Driver_CAN1.MessageRead(0,&rx_msg_info,data_buf,2);
 		identifiant = rx_msg_info.id;
 		retour=data_buf[0];
 		retour=data_buf[1];
-		taille=rx_msg_info.dlc;
-		
-		sprintf(texte,"%d   %d ",retour,retour2);
 		
 		choisir_music(retour,retour2);
 
